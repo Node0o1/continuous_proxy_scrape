e4ab1617bbb69478ec501053c9a7f849a4f62133
Node001 <node0o1@proton.me>
2024-07-15 18:49:58 -0400

    Update continuous_proxy_scrape.py

--- continuous_proxy_scrape.py
+++ continuous_proxy_scrape.py
@@ -16,32 +16,42 @@ import time
 
 PROXY_SITE_URL:str = "#####################"
 OF_HANDLE:str = 'proxies.bin'
-SLEEP_TIME:int = 1800 #30 minutes
-START_FILE_READ:tuple = (0, 0)
+SLEEP_TIME:int = 3600; #1hr
+START_FS_POINT:tuple = (0x00, 0x00)
 
 def write_file(proxies:list):
+    unique:int = 0x00
     with open(OF_HANDLE, mode='ab+') as fhandle:
         for i, x in enumerate(proxies, start=0):
-            fhandle.seek(*START_FILE_READ)
-            if(not re.search(x, fhandle.read().decode('utf-8', errors='ignore'))):
-                fhandle.write((f'{x}\n').encode('utf-8'))      
+            fhandle.seek(*START_FS_POINT)
+            if(not re.search(x, fhandle.read().decode('utf-8','ignore'))):
+                fhandle.write((f'{x}{chr(0x0a)}').encode('utf-8')); 
+                unique+=0x01
+    return unique    
                 
 def get_https_proxies():
-    raw_data:object = sp(requests.get(PROXY_SITE_URL).text, 'html.parser')
+    raw_data:object = sp(requests.get(PROXY_SITE).text, 'html.parser')
     textarea:object = raw_data.find('textarea')
-    proxies:list = re.findall('([0-9\S]*\:0?[0-9]*)',str(textarea))[1:]
-    write_file(proxies)
+    proxies:list = re.findall(r'([0-9\S]*\:0?[0-9]*)',str(textarea))[1:]
+    proxies_written:int = write_file(proxies)
+    return proxies_written
 
 def main():
-    print('\nGathering proxies...\nPress "ctl + c" to quit or close the window.')
+    iteration_count:int = 0x01
+    total_proxies:int = 0x00
+    print('\nGathering proxies...')
+    
+    
     while(True):
         try:
-            get_https_proxies()
-            #pause between iterations
+            new_proxies:int = get_https_proxies()
+            print('='*25)
+            print(f'Scrape Iteration: {iteration_count}')
+            print(f'Unique Proxies Found: {new_proxies}')
+            print(f'Total Unique Proxies: {total_proxies}')
+            print('='*25)
+            iteration_count+=0x01
+            print('Press "ctl + c" to quit scraping...')
             time.sleep(SLEEP_TIME)
-        except KeyboardInterrupt:
-            break
-    print('exiting')
-    
-if __name__=='__main__':
-    main()
+        except KeyboardInterrupt: break
+    print('Exiting')
